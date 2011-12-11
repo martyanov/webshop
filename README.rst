805ca3bc9c6b9a21fd2b0fa06027e0ef7ffca3bb
Andrey V. Martyanov <andrey.v.martyanov@gmail.com>
2011-12-12 04:20:42 +0500

    Moving to Flask: updated documentation and setup.py

---
+++ README.rst
@@ -0,0 +1,9 @@
+=======
+WebShop
+=======
+
+The details can be found `here <https://www.epointsystem.org/trac/vending_machine/wiki/WebShop>`_.
+
+At the moment the project consists of two separate web applications based on `Flask <http://flask.pocoo.org/>`_:
+1. paymentform - for paying invoices
+2. mailreceipt - for sending out emails with receipts
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='webshop',
-    version='0.1.7',
+    version='0.1a',
     packages=['paymentform', 'paymentform.i18n', 'mailreceipt',
         'mailreceipt.i18n','webshop'],
     package_data={'paymentform': ['static/epoint.ico', 'static/style.css'],
                   'mailreceipt': ['static/epoint.ico', 'static/style.css']},
     install_requires=[
-        "cherrypy",
-        "python-gnupg",
+        "Flask>=0.8",
+        "python-gnupg>=0.2.8",
     ],
     test_suite = "tests",
     description='Bob is selling various physical and digital goods over http/https',
